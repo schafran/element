78be9115c3b1379231c1370864b3be3e639dfbce
Bajohr, Rayk <rayk.bajohr@siemens.com>
2025-10-07 13:53:00 +0200

    fix(ip-input): ensure validation verifies the entire input string

--- projects/element-ng/ip-input/address-validators.ts
+++ projects/element-ng/ip-input/address-validators.ts
@@ -18,19 +18,24 @@ const ipV6CIDRRegex =
   /^(([0-9A-Fa-f]{1,4}:){7})([0-9A-Fa-f]{1,4})\/([1-9]|[1-9][0-9]|10[0-9]|11[0-9]|12[0-8])$/;
 
 // Matches
-// 1:2:3:4:5:6:7:8
-// 1::             (1:0:0:0:0:0:0:0)
-// 1:2:3:4:5:6:7:: (1:2:3:4:5:6:7:0)
-// 1::8            (1:0:0:0:0:0:0:8)
-// 1:2:3:4:5:6::8  (1:2:3:4:5:6:0:8)
-// 1::7:8          (1:0:0:0:0:0:7:8)
-// 1:2:3:4:5::7:8  (1:2:3:4:5:0:7:8)
-// ...
+// 1:2:3:4:5:6:7:8              (Full)
+// 1:: 1:2:3:4:5:6:7::          (Leading/Trailing compressions)
+// 1::8 1:2:3:4:5:6::8          (1 block compressed)
+// 1::7:8 1:2:3:4:5::7:8        (2 blocks compressed)
+// 1::6:7:8 1:2:3:4::6:7:8      (3 blocks compressed)
+// 1::5:6:7:8 1:2:3::5:6:7:8    (4 blocks compressed)
+// 1::4:5:6:7:8 1:2::4:5:6:7:8  (5 blocks compressed)
+// 1::3:4:5:6:7:8               (6 blocks compressed)
+// ::3:4:5:6:7:8
+// ::8                          (7 blocks compressed)
+// ::                           (All-zeros address)
+// IPv4-Embedded / IPv4-Mapped Address (e.g., ::ffff:192.0.2.1)
+// see https://stackoverflow.com/questions/53497/regular-expression-that-matches-valid-ipv6-addresses
 const ipV6ZeroCompressionRegex =
-  /^(([0-9A-Fa-f]{1,4}:){7})([0-9A-Fa-f]{1,4})|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)$/;
+  /^(([0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4}|([0-9A-Fa-f]{1,4}:){1,7}:|([0-9A-Fa-f]{1,4}:){1,6}:[0-9A-Fa-f]{1,4}|([0-9A-Fa-f]{1,4}:){1,5}(:[0-9A-Fa-f]{1,4}){1,2}|([0-9A-Fa-f]{1,4}:){1,4}(:[0-9A-Fa-f]{1,4}){1,3}|([0-9A-Fa-f]{1,4}:){1,3}(:[0-9A-Fa-f]{1,4}){1,4}|([0-9A-Fa-f]{1,4}:){1,2}(:[0-9A-Fa-f]{1,4}){1,5}|[0-9A-Fa-f]{1,4}:((:[0-9A-Fa-f]{1,4}){1,6})|::((?:[0-9A-Fa-f]{1,4}:){1,6})[0-9A-Fa-f]{1,4}|::[0-9A-Fa-f]{1,4}|::|((([0-9A-Fa-f]{1,4}:){6}|::([0-9A-Fa-f]{1,4}:){5}|[0-9A-Fa-f]{1,4}::([0-9A-Fa-f]{1,4}:){4}|([0-9A-Fa-f]{1,4}:){1,2}::([0-9A-Fa-f]{1,4}:){3}|([0-9A-Fa-f]{1,4}:){1,3}::([0-9A-Fa-f]{1,4}:){2}|([0-9A-Fa-f]{1,4}:){1,4}::[0-9A-Fa-f]{1,4}:|([0-9A-Fa-f]{1,4}:){1,5}::)((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])))$/;
 
 const ipV6ZeroCompressionCIDRRegex =
-  /^((([0-9A-Fa-f]{1,4}:){7})([0-9A-Fa-f]{1,4})|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:))\/([1-9]|[1-9][0-9]|10[0-9]|11[0-9]|12[0-8])$/;
+  /^(([0-9A-Fa-f]{1,4}:){7}[0-9A-Fa-f]{1,4}|([0-9A-Fa-f]{1,4}:){1,7}:|([0-9A-Fa-f]{1,4}:){1,6}:[0-9A-Fa-f]{1,4}|([0-9A-Fa-f]{1,4}:){1,5}(:[0-9A-Fa-f]{1,4}){1,2}|([0-9A-Fa-f]{1,4}:){1,4}(:[0-9A-Fa-f]{1,4}){1,3}|([0-9A-Fa-f]{1,4}:){1,3}(:[0-9A-Fa-f]{1,4}){1,4}|([0-9A-Fa-f]{1,4}:){1,2}(:[0-9A-Fa-f]{1,4}){1,5}|[0-9A-Fa-f]{1,4}:((:[0-9A-Fa-f]{1,4}){1,6})|::((?:[0-9A-Fa-f]{1,4}:){1,6})[0-9A-Fa-f]{1,4}|::[0-9A-Fa-f]{1,4}|::|((([0-9A-Fa-f]{1,4}:){6}|::([0-9A-Fa-f]{1,4}:){5}|[0-9A-Fa-f]{1,4}::([0-9A-Fa-f]{1,4}:){4}|([0-9A-Fa-f]{1,4}:){1,2}::([0-9A-Fa-f]{1,4}:){3}|([0-9A-Fa-f]{1,4}:){1,3}::([0-9A-Fa-f]{1,4}:){2}|([0-9A-Fa-f]{1,4}:){1,4}::[0-9A-Fa-f]{1,4}:|([0-9A-Fa-f]{1,4}:){1,5}::)((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])))((\/((1[0-1][0-9])|(12[0-8])|([0-9]|[1-9][0-9]))))$/;
 
 /**
  * Validator factory for a IPV6 address.
@@ -40,19 +45,19 @@ export const ipV6Validator = (options: {
   zeroCompression?: boolean;
   cidr?: boolean;
 }): ValidatorFn => {
+  const match = (i: string, r: RegExp): boolean => !!i.match(r);
   return (control: AbstractControl): ValidationErrors | null => {
-    const value = control.value;
+    const value = control.value ?? '';
     let valid: boolean | RegExpMatchArray | null | undefined;
     if (options.cidr && options.zeroCompression) {
-      valid = value === '' || value?.match(ipV6ZeroCompressionCIDRRegex);
+      valid = value === '' || match(value, ipV6ZeroCompressionCIDRRegex);
     } else if (options.cidr) {
-      valid = value === '' || value?.match(ipV6CIDRRegex);
+      valid = value === '' || match(value, ipV6CIDRRegex);
     } else if (options.zeroCompression) {
       valid =
-        (value === '' || value?.toString().match(ipV6ZeroCompressionRegex)) &&
-        value?.split('::').length <= 2;
+        value === '' || (match(value, ipV6ZeroCompressionRegex) && value?.split('::').length <= 2);
     } else {
-      valid = value === '' || value?.toString().match(ipV6Regex);
+      valid = value === '' || match(value, ipV6Regex);
     }
     return valid ? null : { ipv6Address: true };
   };
